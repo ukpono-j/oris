0fd3c3e5feb0ffc92b9c9f444986cd857438fd97
ukpono-j <ukponoakpan270@gmail.com>
2022-11-29 16:02:14 +0100

    making changes the first page by adding error messages and making api calls the firebase database

--- js/app.js
+++ js/app.js
@@ -1,18 +1,42 @@
-const formEl = document.querySelector('.main-form');
+// fetch("https://new-oris-default-rtdb.firebaseio.com")
+// .then((resp)=>console.log(resp))
+// .catch(err=>console.log(err))
 
-// formEl.addEventListener('submit',  event = >{
-//     event.preventDefault()
-// })
 
-formEl.addEventListener("submit", event => {
-    event.preventDefault()
+const userName = document.querySelector("#name");
+const telephone = document.querySelector("#telephoneid");
+const cars = document.querySelector("#carsid");
+const interestTwo = document.querySelector("#interestTwoId");
+const submitButton = document.querySelector("#submit");
 
-    const formData = new FormData(formEl);
-    const data = new URLSearchParams(formData)
-    //    console.log(formData.get('username'))
-    fetch('https://regress.in/api/users', {
-        method: "POST",
-        body: data
-    }).then(res => res.json())
-       
+const receiver_name = document.querySelector(".receiver_name")
+const receiver_telephone = document.querySelector(".receiver_telephone")
+const receiver_cars = document.querySelector(".receiver_cars")
+const receiver_interest = document.querySelector(".receiver_interest")
+
+
+
+const data = {
+    userName,
+    telephone,
+    cars,
+    interestTwo,
+}
+
+
+
+submitButton.addEventListener("click", ()=>{
+
+localStorage.setItem("userName", userName.value);
+localStorage.setItem("telephone", telephone.value);
+localStorage.setItem("cars",cars.value);
+localStorage.setItem("interestTwo", interestTwo.value);
+
+
+
+
+
 })
+
+
+
